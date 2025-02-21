2ff80ac138364ac6052959de597df83a974826d1
Rahul Gupta <rahul1990gupta@gmail.com>
2025-02-21 19:13:52 +0530

    adds description to the problem

--- practice/python/src/c01_fizzbuzz.py
+++ practice/python/src/c01_fizzbuzz.py
@@ -1,3 +1,12 @@
+"""
+This is the classical Fizzbuzz challenge
+for a given number n, return 'Fizz' if n is divisible by 3,
+return 'Buzz' if n is divisible by 5,
+return 'FizzBuzz' if n is divisible by both 3 and 5,
+otherwise return the number as a string.
+"""
+
+
 def fizzbuzz(n):
     if n % 15 == 0:
         return 'FizzBuzz'
--- practice/python/src/c02_matrotate.py
+++ practice/python/src/c02_matrotate.py
@@ -1,2 +1,15 @@
+"""
+Given an array, rotate the array 90 degrees clockwise.
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+rotated matrix = [
+    [3, 6, 9],
+    [2, 5, 8],
+    [1, 4, 7]
+]
+"""
 def rotate_counter_clockwise(arr):
     pass
