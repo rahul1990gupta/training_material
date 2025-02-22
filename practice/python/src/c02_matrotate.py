3eab9dc4abcfb3b404fbf49eb22e53cfe2b6ec45
aryansamaria <aryansamaria44@gmail.com>
2025-02-22 14:32:39 +0530

    question update

--- practice/python/src/c02_matrotate.py
+++ practice/python/src/c02_matrotate.py
@@ -13,11 +13,4 @@ rotated matrix = [
 """
 
 def rotate_counter_clockwise(arr):
-    n = len(arr)
-
-    narr = [[0 for _ in range(n)] for _ in range(n)]
-
-    for i in range(n):
-        for j in range(n):
-            narr[n-1-j][i] = arr[i][j]
-    return narr        
+    pass       
--- practice/python/src/c03_fibonacci.py
+++ practice/python/src/c03_fibonacci.py
@@ -2,13 +2,6 @@
 # Print fibbonaci series up to n using recursion and return the output in List
 
 
-def fibonacci(n,c=0,a=0,b=1,l=None):
-    if l is None:
-        l=[]
+def fibonacci(n):
+    pass
     
-    if c==n:
-        return l
-    l.append(a)
-    return fibonacci(n,c+1,b,a+b,l)
-    
-print(fibonacci(10))
